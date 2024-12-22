a029106138e28d4ee9e8aa446559877031b6e081
Asad Rana <asadlukman246@gmail.com>
2024-12-22 21:31:32 +0530

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Home from './Pages/Home/Home'
-import MessageInterFace from './Pages/messageInterFace'
+import MessageInterFace from './Pages/MessageInterFace'
 function App() {
   return (
     <Router>
